661007e94106bfbea5badc364263f9300075cd6b
codyowl <codyowl@gmail.com>
2020-07-28 21:46:28 +0530

    user registration and login forms added to django user registration login

--- user-registrations-login/user_registration_login/accounts/views.py
+++ user-registrations-login/user_registration_login/accounts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect, HttpResponse
+from accounts.forms import UserRegisterForm, LoginForm
 
-# Create your views here.
+
