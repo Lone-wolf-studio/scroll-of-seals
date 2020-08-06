ac5719b2a39545ef7fde1c292a0ffcb65016170d
codyowl <codyowl@gmail.com>
2020-08-06 22:28:58 +0530

    user register view added without confirmation logic :rocket:

--- user-registrations-login/user_registration_login/accounts/views.py
+++ user-registrations-login/user_registration_login/accounts/views.py
@@ -5,3 +5,20 @@ from django.http import HttpResponseRedirect, HttpResponse
 from accounts.forms import UserRegisterForm, LoginForm
 
 
+def register(request, template="accounts/register.html"):
+	if request.method == 'POST':
+		form = UserRegisterForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data['username']
+			first_name = form.cleaned_data['first_name']
+			password = form.cleaned_data['password']
+			email = form.cleaned_data['email']
+			user = User(username=username, email=email,
+						first_name=first_name)
+			user.set_password(password)
+			user.is_active = False
+			user.save()
+			return HttpResponseRedirect('/login')
+	else:
+		form = UserRegisterForm()        
+	return render(request, template, context)
